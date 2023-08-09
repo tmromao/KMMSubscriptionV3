280b62189b5049426112d82b932354f89267de69
tromao <tiagomromao@gmail.com>
2023-08-09 12:21:49 +0100

    Changed, downgraded Material3 to Material jetbrains.compose.material

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -39,7 +39,7 @@ kotlin {
             dependencies {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
-                implementation(compose.material3)
+                implementation(compose.material)
                 implementation(compose.materialIconsExtended)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
